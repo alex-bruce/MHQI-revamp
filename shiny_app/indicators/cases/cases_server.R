bb3a8dcbbf0e7d8644739f099c8980bfed1816c3
vly12 <vay.ly@phs.scot>
2025-09-25 14:41:15 +0000

    Removed the COVID Cases plot and made a new 3 season line chart for covid cases

--- shiny_app/indicators/cases/cases_server.R
+++ shiny_app/indicators/cases/cases_server.R
@@ -34,32 +34,32 @@ altTextServer("ons_cases_modal",
 #                                 tags$li("There have been peaks throughout the pandemic, notably in",
 #                                         "Sep 2021, Dec 2021, Mar 2022 and Jun 2022")))
 
-altTextServer("reported_cases_modal",
-              title = "Reported COVID-19 cases",
-              content = tags$ul(tags$li("This is a plot of the number of reported COVID-19 cases each week."),
-                                tags$li("The x axis is the week ending date"),
-                                tags$li("The y axis is the number of reported cases"),
-                                tags$li("There is a navy blue trace which shows the number of reported cases each week."),
-                                tags$li("There are two vertical lines: the first denotes that prior to 5 Jan 2022 ",
-                                        "reported cases are PCR only, and since then they include PCR and LFD cases; ",
-                                        "the second marks the change in testing policy on 1 May 2022.")
-              )
-)
+#altTextServer("reported_cases_modal",
+              #title = "Reported COVID-19 cases",
+              #content = tags$ul(tags$li("This is a plot of the number of reported COVID-19 cases each week."),
+                               # tags$li("The x axis is the week ending date"),
+                                #tags$li("The y axis is the number of reported cases"),
+                                #tags$li("There is a navy blue trace which shows the number of reported cases each week."),
+                               # tags$li("There are two vertical lines: the first denotes that prior to 5 Jan 2022 ",
+                                        #"reported cases are PCR only, and since then they include PCR and LFD cases; ",
+                                        #"the second marks the change in testing policy on 1 May 2022.")
+            #  )
+#)
 
-output$reported_cases_table <- renderDataTable({
-  Cases_Weekly %>%
-    mutate(WeekEnding = convert_opendata_date(WeekEnding)) %>%
-    dplyr::rename(`Reported cases` = NumberCasesPerWeek) %>%
-    select(WeekEnding, `Reported cases`) %>%
-    arrange(desc(WeekEnding)) %>%
-    make_table(add_separator_cols = c(2), order_by_firstcol = "desc")
-})
+#output$reported_cases_table <- renderDataTable({
+  #Cases_Weekly %>%
+    #mutate(WeekEnding = convert_opendata_date(WeekEnding)) %>%
+    #dplyr::rename(`Reported cases` = NumberCasesPerWeek) %>%
+    #select(WeekEnding, `Reported cases`) %>%
+    #arrange(desc(WeekEnding)) %>%
+    #make_table(add_separator_cols = c(2), order_by_firstcol = "desc")
+#})
 
-output$reported_cases_plot <- renderPlotly({
-  Cases_Weekly %>%
-    make_weekly_reported_cases_plot()
+#output$reported_cases_plot <- renderPlotly({
+  #Cases_Weekly %>%
+    #make_weekly_reported_cases_plot()
 
-})
+#})
 
 
 output$ons_cases_plot <- renderPlotly({
@@ -68,6 +68,32 @@ output$ons_cases_plot <- renderPlotly({
 
 })
 
+output$covid_line_plot <- renderPlotly({
+
+  covid_cases_weekly %>%
+    create_covid_line_chart()
+})
+
+# COVID Rates per 100,000 table
+output$covid_cases_table <- renderDataTable({
+  covid_cases_weekly %>%
+    arrange(desc(WeekEnding)) %>%
+    select(Season, ISOWeek, RatePer100000) %>%
+    mutate(Season = factor(Season),
+           ISOWeek = factor(ISOWeek)) %>%
+    rename(`ISO Week` = ISOWeek,
+           `Rate per 100,000` = RatePer100000) %>%
+    make_table(add_separator_cols_1dp = c(3),
+               filter_cols = c(1,2))
+})
+
+altTextServer("reported_cases_per_100k",
+title = "Reported COVID-19 cases per 100,0000 people",
+content = tags$ul(tags$li("This is a plot shows the past 3 seasons of COVID-19 cases per 100,000 people."),
+ tags$li("The x axis is the week number"),
+tags$li("The 3 lines show a separate season respectively. The legend indicates which line corresponds to which season.")
+  )
+)
 
 # output$wastewater_plot <- renderPlotly({
 #   Wastewater %>%
