05891602e19fa2c07723a1d514876a5fe2aa591d
Mark <mark.hamilton4@phs.scot>
2026-01-15 16:09:03 +0000

    Updating input data for covid cases/tests open data

--- Dashboard Data Transfer/Transfer Scripts/new_od_tests_raw_data.R
+++ Dashboard Data Transfer/Transfer Scripts/new_od_tests_raw_data.R
@@ -53,22 +53,26 @@ FLU_agGp_tests_a<-Flu_AorB_AgeGp_pos %>%
 
 #i_covid19_all_cases_tests_data created in Raw_data_CASES.R script
 
-i_covid19_PCR_data_raw<-i_covid19_all_cases_tests_data%>% 
-  #only pick PCR tests (remove LFD)
-  filter(test_type=="PCR" &
-           Date>=COVID_19_start_date$WeekBeginning)%>% 
-  select(specimen_id,Date,Sex,reporting_health_board,local_authority,
-         test_type,test_result,
-         test_result_record_source,postcode,AgeGroup) %>% 
-  #get isoweek start and end
-  merge(date_reference %>% 
-          select(Date=date,ISOweek_beginning,ISOweek_ending),
-        by="Date") %>% 
-  #remove out of Scotland, no fixed abode & NA HBs by merging with HB template
-  left_join(HB_tests,by="reporting_health_board") %>% 
-  filter(!is.na(HBcode)) %>%
-  filter(Date>=COVID_19_start_date$WeekBeginning,
-         Date <=od_sunday) 
+# i_covid19_PCR_data_raw<-i_covid19_all_cases_tests_data%>% 
+#   #only pick PCR tests (remove LFD)
+#   filter(test_type=="PCR" &
+#            Date>=COVID_19_start_date$WeekBeginning)%>% 
+#   select(specimen_id,Date,Sex,reporting_health_board,local_authority,
+#          test_type,test_result,
+#          test_result_record_source,postcode,AgeGroup) %>% 
+#   #get isoweek start and end
+#   merge(date_reference %>% 
+#           select(Date=date,ISOweek_beginning,ISOweek_ending),
+#         by="Date") %>% 
+#   #remove out of Scotland, no fixed abode & NA HBs by merging with HB template
+#   left_join(HB_tests,by="reporting_health_board") %>% 
+#   filter(!is.na(HBcode)) %>%
+#   filter(Date>=COVID_19_start_date$WeekBeginning,
+#          Date <=od_sunday) 
+
+# Read in OD Cases file 
+od_tests_file <- read_all_excel_sheets(glue(input_data, 
+                                            "COVID-19 Tests - File for Open Data {format(report_date-1, format='%d%m%Y')}.xlsx"))
 
 gc()
 
