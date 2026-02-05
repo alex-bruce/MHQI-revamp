a5756b4e60b64fe8a716f61dcfde0a75c5799547
Carrie Turner <carrieann26@outlook.com>
2026-02-05 15:28:15 +0000

    formatting of wastewater tabs

--- shiny_app/indicators/wastewater/health_board/hb_server.R
+++ shiny_app/indicators/wastewater/health_board/hb_server.R
@@ -20,8 +20,8 @@ output$health_board_plot =  renderPlotly({
       marker = list(color = "#3F3685"),
       name = input$selected_board,
       text = ~paste("Week Ending Date:", format(End, "%d %b %y"),
-                    "<br>Average (Mgc/p/d):", round_half_up(average, 2), 
-                    "<br>Coverage:", paste0(round_half_up(coverage, 2)*100, "%")),
+                    "<br>Average (Mgc/p/d):", round_half_up(average, 1), 
+                    "<br>Coverage:", paste0(round_half_up(coverage, 1)*100, "%")),
       hoverinfo = "text"
     ) %>%
     # Add the Scotland data
@@ -35,8 +35,8 @@ output$health_board_plot =  renderPlotly({
       marker = list(color = "green"),
       name = 'Scotland',
       text = ~paste("Week Ending Date:", format(End, "%d %b %y"), 
-                    "<br>Scotland Average (Mgc/p/d):", round_half_up(average, 2), 
-                    "<br>Scotland Coverage:", paste0(round_half_up(coverage, 2)*100, "%")),
+                    "<br>Scotland Average (Mgc/p/d):", round_half_up(average, 1), 
+                    "<br>Scotland Coverage:", paste0(round_half_up(coverage, 1)*100, "%")),
       hoverinfo = "text"
     ) %>%
     add_lines_and_notes(dataframe = hb_all_values,
@@ -84,6 +84,8 @@ output$health_board_table <- renderDataTable({
     dplyr::rename('NHS Health Board' = health_board) %>% 
     dplyr::rename('Average (Mgc/p/d)' = average) %>%
     dplyr::rename('Coverage (%)' = coverage) %>% 
-    make_table(order_by_firstcol = "desc")
+    make_table(order_by_firstcol = "desc",
+               add_separator_cols = 4,
+               add_separator_cols_1dp = 3)
   
 })
